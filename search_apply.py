276e1d5b7e510265d596d1b741cca699ff0ec221
Aditya9501 <130274063+Aditya9501@users.noreply.github.com>
2023-08-23 23:29:36 +0530

    Update search_apply.py

--- search_apply.py
+++ search_apply.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import pandas as pd
+import datetime
 import time
 from time import sleep
 from selenium.webdriver.common.by import By
@@ -7,25 +8,27 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import os
+
+naukri = "https://www.naukri.com"
+log_in_layer = (By.XPATH,"//a[@id='login_Layer']")
+username_details = (By.XPATH,"//input[@placeholder='Enter your active Email ID / Username']")
+password_details = (By.XPATH, "//input[@placeholder='Enter your password']")
+log_in_click = (By.XPATH,"//button[@type='submit']")
+wait = WebDriverWait(driver,5)
+chain = ActionChains(driver)
 
 def get_joblink_with_login(user,passcode,jobdesgn,yrsexp):
     driver = webdriver.Chrome()
-    driver.get("https://www.naukri.com")
+    driver.get(naukri)
     driver.maximize_window()
 
-    wait = WebDriverWait(driver,10)
-    chain = ActionChains(driver)
-
     # Click on Log-In
-    log_in_layer = (By.XPATH,"//a[@id='login_Layer']")
     wait.until(EC.element_to_be_clickable(log_in_layer)).click()
 
     # Input your Detail
-    username_deatails = (By.XPATH,"//input[@placeholder='Enter your active Email ID / Username']")
-    password_details = (By.XPATH, "//input[@placeholder='Enter your password']")
-    log_in_click = (By.XPATH,"//button[@type='submit']")
-
-    wait.until(EC.visibility_of_element_located(username_deatails)).send_keys(user)
+    u_d = (By.XPATH,"//input[@placeholder='Enter your active Email ID / Username']")
+    wait.until(EC.visibility_of_element_located(u_d)).send_keys(user)
     wait.until(EC.visibility_of_element_located(password_details)).send_keys(passcode)
     wait.until(EC.element_to_be_clickable(log_in_click)).click()
     
@@ -36,7 +39,6 @@ def get_joblink_with_login(user,passcode,jobdesgn,yrsexp):
     job_type_loc = (By.XPATH, "//input[@placeholder='Enter keyword / designation / companies']")
     wait.until(EC.element_to_be_clickable(job_type_loc)).send_keys(jobdesgn)
 
-    
     # Print Similar items
     simi_jobs = (By.XPATH,"//div[contains(@class,'nI-gNb-search-bar')]//li//div")
     wait.until(EC.visibility_of_all_elements_located(simi_jobs))
@@ -69,7 +71,7 @@ def get_joblink_with_login(user,passcode,jobdesgn,yrsexp):
                     "Deep Learning","Natural Language","Computer Vision","Data Mining","Power BI", "Tableau","Data Analysis","Data Modeling",
                     "Data Wrangling","Business Analyst"]
     keyword_title2 = ["ML", "AI", "CV", "NLP","SQL"]
-    not_keywords = ["Developer","Django","Stack","Config","Server","Admin"]
+    not_keywords = ["Developer","Django","Stack","Config","Server","Admin","Associate","Content"]
     
     job_link = []
     job_title = []
@@ -82,13 +84,14 @@ def get_joblink_with_login(user,passcode,jobdesgn,yrsexp):
         jobs = driver.find_elements(By.XPATH, "//article")
 
         for j in range(len(jobs)):
-            Title = driver.find_element(By.XPATH, f'//article[{j+1}]/div[1]/div[1]/a[1]').text
-            if (any(item.lower() in Title.lower() for item in keyword_title) or any(item in Title for item in keyword_title2)) and not any(item.lower() in Title.lower() for item in not_keywords):
+            job_posting = driver.find_element(By.XPATH, f'//article[{j+1}]/div[1]/div[1]/a[1]').text
+            if (any(item.lower() in job_posting.lower() for item in keyword_title) or any(item in job_posting for item in keyword_title2)) and not any(item.lower() in job_posting.lower() for item in not_keywords):
                 job_link.append(driver.find_element(By.XPATH, f'//article[{j+1}]/div[1]/div[1]/a[1]').get_attribute('href'))
                 job_title.append(driver.find_element(By.XPATH, f'//article[{j+1}]/div[1]/div[1]/a[1]').text)
-
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='fright fs14 btn-secondary br2']"))).click()
-
+        try:
+            wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='fright fs14 btn-secondary br2']"))).click()
+        except Exception as e:
+            print('No job further')
         i += 1
     driver.close()
 
@@ -96,83 +99,97 @@ def get_joblink_with_login(user,passcode,jobdesgn,yrsexp):
     
     return job_df
 
-def get_keyskill_up(user,passcode,df):
+def get_keyskill(user,passcode,df):
     main_skill = []
-    url = "https://www.naukri.com"
+    main_skill1 = []
+    url = naukri
     driver = webdriver.Chrome()
     driver.get(url)
     driver.maximize_window()
-
     wait = WebDriverWait(driver,10)
 
-    log_in_layer = (By.XPATH, "//a[@id='login_Layer']")
     wait.until(EC.element_to_be_clickable(log_in_layer)).click()
 
     # Input your Details
-    username_details = (By.XPATH, "//input[@placeholder='Enter your active Email ID / Username']")
-    password_details = (By.XPATH, "//input[@placeholder='Enter your password']")
-    log_in_click = (By.XPATH, "//button[@type='submit']")
-
-    wait.until(EC.visibility_of_element_located(username_details)).send_keys(user)
+    u_d = (By.XPATH,"//input[@placeholder='Enter your active Email ID / Username']")
+    wait.until(EC.visibility_of_element_located(u_d)).send_keys(user)
     wait.until(EC.visibility_of_element_located(password_details)).send_keys(passcode)
     wait.until(EC.element_to_be_clickable(log_in_click)).click()
 
     time.sleep(2)
-    
+
     for joburl in df.URL.values:
         driver.get(joburl)
-    
-        try:
-            wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='getJobKeySkillsSection key-skill']/div/a/span")))
-            tot_skill = driver.find_elements(By.XPATH, "//div[@class='getJobKeySkillsSection key-skill']/div/a/span")
-            keyskills = [i.text for i in tot_skill]
-
+        dicti = {1: "div[contains(@class,'key-skill')]", 2: "div[contains(@class,'getJobKeySkillsSection key-skill')]", 3: "div[contains(@class,'styles_key-skill__GIPn_')]"}
+        for loc in dicti:
             try:
-                highlighted = driver.find_elements(By.XPATH, "//div[@class='getJobKeySkillsSection key-skill']/div/a/em")
-                a = len(highlighted)
-            except Exception as e:
-                a = 'all'
-
-            main_skill.append((keyskills, a, "Not Possible",joburl))
-
-        except TimeoutException as e1:
-            try:
-                wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='key-skill']/div/a/span")))
-                tot_skill = driver.find_elements(By.XPATH, "//div[@class='key-skill']/div/a/span")
+                wait.until(EC.visibility_of_all_elements_located((By.XPATH, f"//{dicti[loc]}/div/a/span")))
+                tot_skill = driver.find_elements(By.XPATH, f"//{dicti[loc]}/div/a/span")
                 keyskills = [i.text for i in tot_skill]
 
                 try:
-                    highlighted = driver.find_elements(By.XPATH, "//div[@class='key-skill']/div/a/em")
+                    if dicti[loc] == "div[contains(@class,'styles_key-skill__GIPn_')]":
+                        highlighted = driver.find_elements(By.XPATH, f"//{dicti[loc]}/div/a/i")
+                    else:
+                        highlighted = driver.find_elements(By.XPATH, f"//{dicti[loc]}/div/a/em")
                     a = len(highlighted)
                 except Exception as e:
                     a = 'all'
-                
+
+            except Exception as e:
+                pass
+            try:
                 button = driver.find_element(By.XPATH, "//button[2]")
-                main_skill.append((keyskills, a, button.text,joburl))
-
-
-            except TimeoutException as e2:
-                try:
-                    wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='styles_key-skill__GIPn_']/div/a/span")))
-                    tot_skill = driver.find_elements(By.XPATH, "//div[@class='styles_key-skill__GIPn_']/div/a/span")
-                    keyskills = [i.text for i in tot_skill]
-
-                    try:
-                        highlighted = driver.find_elements(By.XPATH, "//div[@class='styles_key-skill__GIPn_']/div/a/i")
-                        a = len(highlighted)
-                    except Exception as e:
-                        a = 'all'
-
-                    button = driver.find_element(By.XPATH, "//button[2]")
-                    main_skill.append((keyskills, a, button.text,joburl))
-
-                except TimeoutException as e3:
-                    main_skill.append(('keyskills', 'a', 'button.text'))  # No need to print here since the visibility print statements are removed
-
-        finally:
-            pass
-    apply_df = pd.DataFrame(main_skill)
+                main_skill.append((keyskills, a, button.text, joburl))
+            except:
+                main_skill.append((keyskills, a, "Not Possible", joburl))
+    
+            main_skill = [main_skill[i] for i in range(0,len(main_skill),3)]
+        
+    apply_df = pd.DataFrame(main_skill1)
     apply_df.columns = ['Skills','Highlighted','Response','URL']
     apply_df = pd.merge(left=df,right=apply_df,on='URL',how='inner')
     return apply_df
 
+def autoapply(user,passcode,jobdesgn,yrsexp):
+    
+    job_df = get_joblink_with_login(user,passcode,jobdesgn,yrsexp)
+    print(f'keyskill to serach from {len(job_df.URL.values)} jobs')
+    print(list(job_df.URL.values))
+    apply_df = get_keyskill(user,passcode,job_df)    
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%d%m%y_%I%M%p")
+
+    filename = formatted_datetime+'_'+jobdesgn+'.csv'
+    path = r'C:\Users\Aditya\Desktop\Naukri\Search_Apply'
+    apply_df.to_csv(os.path.join(path,filename),index=False)
+
+    now_apply = apply_df['URL'][apply_df['Response']=='Apply'].values
+    
+    print(f'We can appply to {len(now_apply)} jobs')
+
+    driver = webdriver.Chrome()
+    driver.get(naukri)
+    driver.maximize_window()
+
+    wait = WebDriverWait(driver,10)
+
+    wait.until(EC.element_to_be_clickable(log_in_layer)).click()
+
+    # Input your Details
+    u_d = (By.XPATH,"//input[@placeholder='Enter your active Email ID / Username']")
+    wait.until(EC.visibility_of_element_located(u_d)).send_keys(user)
+    wait.until(EC.visibility_of_element_located(password_details)).send_keys(passcode)
+    wait.until(EC.element_to_be_clickable(log_in_click)).click()
+
+    time.sleep(2)
+
+    for joburl in now_apply:
+        driver.get(joburl)
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, applied_button))).click()
+            print('Already Applied to the job')
+        except:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, to_apply_button))).click()
+
+    driver.quit()
